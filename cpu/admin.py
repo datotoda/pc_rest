f9c8bf1b47cbc28927a3dd02849427a1dd8a52aa
datotoda <datotoda1342@gmail.com>
2022-11-25 19:02:19 +0400

    Add models in cpu app admin
    models: Cpu, Manufacturer, Series, Socket, SocketType

--- cpu/admin.py
+++ cpu/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from cpu.models import Cpu, Manufacturer, Series, Socket, SocketType
 
-# Register your models here.
+
+@admin.register(Cpu)
+class CpuAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Socket)
+class SocketAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SocketType)
+class SocketTypeAdmin(admin.ModelAdmin):
+    pass
