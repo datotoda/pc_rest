ac9ea18b0e9f0ce2b7ddc7f6d0ee218f20264b08
datotoda <datotoda1342@gmail.com>
2022-11-25 18:14:13 +0400

    Add cpu app in settings

--- pc_rest/settings.py
+++ pc_rest/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My Apps
+    'cpu.apps.CpuConfig',
 ]
 
 MIDDLEWARE = [
