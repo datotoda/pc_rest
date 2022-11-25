f81b7d24c5b2a33cce39edf9f4962522be2c6f3d
datotoda <datotoda1342@gmail.com>
2022-11-25 19:44:49 +0400

    Create CpuViewSet

--- cpu/views.py
+++ cpu/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from cpu.models import Cpu
+from cpu.serializers import CpuSerializer
 
-# Create your views here.
+
+class CpuViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Cpu.objects.select_related('manufacturer', 'series', 'socket', 'socket__socket_type')
+    serializer_class = CpuSerializer
