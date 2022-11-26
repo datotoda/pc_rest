bd9f95ddcfaa207cb82d12281702ddac19def0a4
datotoda <datotoda1342@gmail.com>
2022-11-26 14:14:10 +0400

    Add cores_detail and random_cpu action in CpuViewSet

--- cpu/views.py
+++ cpu/views.py
@@ -1,4 +1,5 @@
-from django.db.models import F, Count, Max, Min
+from django.db.models import F, Count, Max, Min, Value, Case, When, Q
+from django.db.models.functions import Concat
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -10,8 +11,8 @@ from cpu.serializers import CpuSerializer, CpuShortSerializer
 class CpuViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Cpu.objects.select_related('manufacturer', 'series', 'socket', 'socket__socket_type')
 
-    def get_serializer_class(self):
-        if self.action == 'retrieve':
+    def get_serializer_class(self, detail=False):
+        if self.action in ['retrieve', 'random_cpu']:
             return CpuSerializer
         return CpuShortSerializer
 
@@ -25,4 +26,33 @@ class CpuViewSet(viewsets.ReadOnlyModelViewSet):
             hyperthread_qs.annotate(brand=F('manufacturer__title')).values('brand').annotate(count=Count('id')),
         ]
 
-        return Response([{'additional_data': additional_data}] + serializer.data)
+        return Response({'additional_data': additional_data, 'cpus': serializer.data})
+
+    @action(detail=False)
+    def cores_detail(self, request):
+        cores_detail_data = self.get_queryset().annotate(
+                title=Concat(F('manufacturer__title'), Value(' '), F('series__title'), Value(' '), F('version'))
+            ).values('id', 'title', 'threads').annotate(
+                performance_cores=F('threads') - F('cores'),
+                efficient_cores=Case(
+                    When(Q(manufacturer__title='Intel') & (Q(version__startswith='13') | Q(version__startswith='12')),
+                         then=F('cores') * 2 - F('threads')),
+                    default=None
+                )
+            )
+
+        return Response(cores_detail_data)
+
+    @action(detail=False)
+    def random_cpu(self, request):
+        all_cpu_qs = self.get_queryset()
+        random_cpu_qs = all_cpu_qs.order_by('?').first()
+        serializer = self.get_serializer(random_cpu_qs, many=False)
+
+        additional_data = all_cpu_qs.aggregate(
+            same_manufacturer_cpus_count=Count('id', Q(manufacturer=random_cpu_qs.manufacturer)),
+            same_series_cpus_count=Count('id', Q(series=random_cpu_qs.series)),
+            same_socket_cpus_count=Count('id', Q(socket=random_cpu_qs.socket))
+        )
+
+        return Response({'additional_data': additional_data, 'cpu': serializer.data})
