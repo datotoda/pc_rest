f27c9bf4f6fe2becd218e63daeb1576f47e36efa
datotoda <datotoda1342@gmail.com>
2022-11-30 15:40:48 +0400

    Make randomcpu django custom command

---
+++ cpu/management/commands/randomcpu.py
@@ -0,0 +1,24 @@
+import json
+
+from django.core.management.base import BaseCommand
+
+from cpu.models import Cpu
+from cpu.serializers import CpuSerializer
+
+
+class Command(BaseCommand):
+    help = 'Print random cpu'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--no-indent', action="store_true")
+        parser.add_argument('-i', '--indent', nargs='?', type=int)
+
+    def handle(self, *args, **options):
+        indent = None if options['no_indent'] else options['indent'] if options['indent'] else 4
+
+        print(options)
+
+        cpu = Cpu.objects.order_by('?').first()
+        serializer = CpuSerializer(cpu)
+
+        self.stdout.write(json.dumps(serializer.data, indent=indent))
