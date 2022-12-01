cca12a909c424426508f0bc96285d37a7010b884
datotoda <datotoda1342@gmail.com>
2022-12-01 23:14:26 +0400

    Create token authentication

--- pc_rest/settings.py
+++ pc_rest/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
 
     # Third Party Apps
     'rest_framework',
-    "debug_toolbar",
+    'rest_framework.authtoken',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
--- pc_rest/urls.py
+++ pc_rest/urls.py
@@ -15,13 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from pc_rest import views
 
 urlpatterns = [
     path('cpu/', include('cpu.urls')),
     path('motherboard/', include('motherboard.urls')),
     path('admin/', admin.site.urls),
+    path('profile/', views.ProfileView.as_view()),
+    path('login_token/', obtain_auth_token),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
 
@@ -31,7 +36,7 @@ urlpatterns = [
 def all_urls(request):
     urls = [url := request.build_absolute_uri()]
     for i in range(len(urlpatterns) - 2):
-        urls.append(url + urlpatterns[i].pattern.regex.pattern[1:])
+        urls.append(url + urlpatterns[i].pattern.regex.pattern[1:].rstrip('\\Z'))
     return Response(urls)
 
 
