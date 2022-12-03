4316b09375c1d00c95a4b1bc1a898679bb259333
datotoda <datotoda1342@gmail.com>
2022-12-03 17:47:28 +0400

    Create ActivationToken model

--- apiauth/admin.py
+++ apiauth/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from apiauth.models import ActivationToken
+
+UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name', 'is_active')
+
+
+@admin.register(ActivationToken)
+class ActivationTokenAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('user',)}),
+    )
+
